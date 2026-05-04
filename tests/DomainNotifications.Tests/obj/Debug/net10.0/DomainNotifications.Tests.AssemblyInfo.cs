589047f543f9dede9ab5d88362758591722bdf7c
João Greco <joaogreco.dev@gmail.com>
2026-05-04 17:09:00 -0300

    Update Company to Greco Labs

--- tests/DomainNotifications.Tests/obj/Debug/net10.0/DomainNotifications.Tests.AssemblyInfo.cs
+++ tests/DomainNotifications.Tests/obj/Debug/net10.0/DomainNotifications.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DomainNotifications.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98ac12fac0f691c0803bef517b8654c18eba8ff1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c1fdcee2000b8f677c2c8d4bd49ef48386ada3c")]
 [assembly: System.Reflection.AssemblyProductAttribute("DomainNotifications.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DomainNotifications.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
